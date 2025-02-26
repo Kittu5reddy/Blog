4a939576420c2d56537145faaa89ba93eabdd6d9
Palvai Kaushik Reddy <kaushikpalvai2004@gmail.com>
2025-02-26 06:45:50 +0530

    second commit

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint,redirect,url_for
+from flask import Blueprint,redirect,url_for,render_template
 
 auth=Blueprint("auth",__name__,url_prefix="/auth")
 
 
 @auth.route('/login')
 def loginPage():
-    return "<h1>Login Page</h1>"
+    return render_template('auth/login.html')
 
 @auth.route('/signup')
 def signupPage():
-    return "<h1>Signup Page</h1>"
+    return render_template('auth/signup.html')
 
 
 @auth.route('/logout')
